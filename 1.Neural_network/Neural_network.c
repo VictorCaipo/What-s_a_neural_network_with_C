442f4936834458ea23c6a8c11b0b389338823e9e
Victor Caipo <caipoccoa@gmail.com>
2026-02-09 10:37:55 -0500

    rev3

--- 1.Neural_network/Neural_network.c
+++ 1.Neural_network/Neural_network.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 #include <math.h>
-/*  
-It's no more than defining a new function
-*/
-void sigmoid_function(float* z_value){
-    *z_value = 1.0 / (1.0 + expf(-1.0 * *z_value) );
+
+float sigmoid_function(float z_value){
+    z_value = 1.0 / (1.0 + expf(-1.0 * z_value) );
+    return z_value;
 }
 
 int main(void){
-    float z = 1.555;
-    float* pointer_z = &z;
-    sigmoid_function(pointer_z);
-    printf("The value of z betwen 0 and 1 is: %.3f\n",*pointer_z);
+    float z = 0.92;
+    float new_z = sigmoid_function(z);
+    printf("The value of z betwen 0 and 1 is: %.3f\n",new_z);
 }
